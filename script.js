432cd49cbdb01a0edcf071cf702ef4d156a59b61
MiguelGT1 <miguel.giannotti07@outlook.com>
2023-08-20 12:52:15 -0300

    projeto-imoveis

---
+++ script.js
@@ -0,0 +1,21 @@
+//PARA QUANDO ADICIONAR AS IMAGENS
+
+function previewImages(event) {
+  const previewContainer = document.getElementById("imagePreview");
+  previewContainer.innerHTML = ""; // Limpa a visualização antes de atualizar
+
+  const files = event.target.files;
+
+  for (const file of files) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const image = document.createElement("img");
+      image.src = e.target.result;
+      image.classList.add("preview-image");
+      previewContainer.appendChild(image);
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
